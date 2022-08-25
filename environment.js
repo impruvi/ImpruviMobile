d931ffec3f5a4724464a8d26c7f1a6b6e4f51e3c
John Magnus <johnmagnus97@gmail.com>
2022-08-25 15:20:02 -0700

    overwrite env to point to beta

--- environment.js
+++ environment.js
@@ -14,6 +14,7 @@ const getEnvVars = (env = Constants.manifest.releaseChannel) => {
     // What is __DEV__ ?
     // This variable is set to true when react-native is running in Dev mode.
     // __DEV__ is true when run locally, but false when published.
+    return ENV.beta;
     if (__DEV__) {
         return ENV.beta;
     } else {
